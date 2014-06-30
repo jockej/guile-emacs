c308c819f93c3139458af804b3c6ff848f2fe463
Robin Templeton <robin@terpri.org>
2014-06-30 04:04:22 -0400

    new function `xmalloc_unsafe'
    * src/alloc.c (xmalloc_unsafe): New function.
    
    Conflicts:
    	src/alloc.c
    	src/lisp.h

--- src/alloc.c
+++ src/alloc.c
@@ -232,6 +232,14 @@ xfree (void *block)
   return;
 }
 
+/* Allocate memory, but if memory is exhausted, return NULL instead of
+   signalling an error.  */
+
+void *
+xmalloc_unsafe (size_t size)
+{
+  return GC_MALLOC (size);
+}
 
 /* Other parts of Emacs pass large int values to allocator functions
    expecting ptrdiff_t.  This is portable in practice, but check it to
