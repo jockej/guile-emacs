c7c6d5cfc9ff335add6fbafdd8c5cb40523cf2fa
Robin Templeton <robin@terpri.org>
2014-08-07 17:12:56 -0400

    compare symbol names with `equal'

--- lisp/emacs-lisp/cl-macs.el
+++ lisp/emacs-lisp/cl-macs.el
@@ -1918,8 +1918,8 @@ except that it additionally expands symbol macros."
           (pcase exp
             ((pred symbolp)
              ;; Perform symbol-macro expansion.
-             (when (cdr (assq (symbol-name exp) env))
-               (setq exp (cadr (assq (symbol-name exp) env)))))
+             (when (cdr (assoc (symbol-name exp) env))
+               (setq exp (cadr (assoc (symbol-name exp) env)))))
             (`(setq . ,_)
              ;; Convert setq to setf if required by symbol-macro expansion.
              (let* ((args (mapcar (lambda (f) (cl--sm-macroexpand f env))
@@ -1937,7 +1937,7 @@ except that it additionally expands symbol macros."
              (let ((letf nil) (found nil) (nbs ()))
                (dolist (binding bindings)
                  (let* ((var (if (symbolp binding) binding (car binding)))
-                        (sm (assq (symbol-name var) env)))
+                        (sm (assoc (symbol-name var) env)))
                    (push (if (not (cdr sm))
                              binding
                            (let ((nexp (cadr sm)))
