e94492d5947d64860f1cdfa7f3dc1c6aa7d29acb
Robin Templeton <robin@terpri.org>
2014-09-21 03:53:07 -0400

    guile feature
    * src/fns.c (syms_of_fns): Add `guile' to `features'.

--- src/fns.c
+++ src/fns.c
@@ -4572,6 +4572,7 @@ syms_of_fns (void)
     doc: /* A list of symbols which are the features of the executing Emacs.
 Used by `featurep' and `require', and altered by `provide'.  */);
   Vfeatures = list1 (intern_c_string ("emacs"));
+  Vfeatures = Fcons (intern_c_string ("guile"), Vfeatures);
   DEFSYM (Qsubfeatures, "subfeatures");
   DEFSYM (Qfuncall, "funcall");
 
